6580c05169f70c187c5fd768d8d80a65aeb102e7
mygodG <yejuncheng@sina.com>
2018-10-13 00:50:07 +0800

    first commit

---
+++ Assets/Scripts/DestroyByContract.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContract : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    public int scoreValue;
+    public int healthValue;
+    private GameController gameController;
+
+    private void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if(gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+
+        if(null == gameController)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gameController.GameOver();
+        }
+
+        if (0 < healthValue)
+        {
+            healthValue--;
+            if(0 == healthValue)
+            {
+                gameController.AddScore(scoreValue);
+                Destroy(gameObject);
+            }
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/MoveBackground.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * Screen.width);
+        if (transform.position.x < -6.33f)
+        {
+            transform.position = new Vector3(6.27f, transform.position.y, transform.position.z);
+        }
+    }
+}
